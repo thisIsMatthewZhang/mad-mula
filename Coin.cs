48f2a770b6e6ebf5cae49ab4531692beda257c1a
Matthew Zhang <zhangmatt22@gmail.com>
2026-09-16 22:10:22 -0500

    offset coins above elevated platforms

--- Coin.cs
+++ Coin.cs
@@ -4,9 +4,19 @@ public partial class Coin : AnimatableBody3D
 {
     [Signal]
     public delegate void GrabbedEventHandler();
+    private ShapeCast3D _shapeCast3DChild;
+
+    public override void _Ready()
+    {
+        _shapeCast3DChild = GetNode<ShapeCast3D>("ShapeCast3D");
+    }
     public override void _PhysicsProcess(double delta)
     {
         Spin();
+        if (_shapeCast3DChild.IsColliding())
+        {
+            Position = new Vector3(Position.X, Position.Y + 0.5f, Position.Z);
+        }
     }
 
     public void Remove()
